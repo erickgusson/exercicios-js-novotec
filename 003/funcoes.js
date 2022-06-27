d5a0f6571d91a5adad76b1342126d59344e8da27
ErickVGusson <92764323+ErickVGusson@users.noreply.github.com>
2022-06-27 17:26:59 -0300

    Exercício 3

---
+++ 003/funcoes.js
@@ -0,0 +1,10 @@
+function somar(){
+
+    let n1 = document.getElementById("n1").value
+    let n2 = document.getElementById("n2").value
+
+    let total = n1 + n2
+
+    document.getElementById("resultado").innerHTML = total
+
+}
