79fe78ee0c36d102615b3402ea48b0b6fc40778b
ErickVGusson <92764323+ErickVGusson@users.noreply.github.com>
2022-06-27 18:03:23 -0300

    Exercício 3

--- 003/funcoes.js
+++ 003/funcoes.js
@@ -3,7 +3,7 @@ function somar(){
     let n1 = document.getElementById("n1").value
     let n2 = document.getElementById("n2").value
 
-    let total = n1 + n2
+    let total = parseFloat(n1) + parseFloat(n2)
 
     document.getElementById("resultado").innerHTML = total
 
