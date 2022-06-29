8f4098ef131e843d611d32e6065349e8a6b3bf1d
ErickVGusson <92764323+ErickVGusson@users.noreply.github.com>
2022-06-29 17:14:59 -0300

    Exercício 8

--- 008/funcoes.js
+++ 008/funcoes.js
@@ -7,23 +7,8 @@ function converter() {
 
     let bruto = parseFloat(soma)
 
-    let irTaxa = 0.11
-    let inssTaxa =  0.08
-    let sindicatoTaxa = 0.05
+    let total = parseFloat(bruto)
 
-    let ir = parseFloat(bruto) * parseFloat(irTaxa)
-    let inss = parseFloat(bruto) * parseFloat(inssTaxa)
-    let sindicato = parseFloat(bruto) * parseFloat(sindicatoTaxa)
-    
-    let total = parseFloat(bruto) - parseFloat(ir) - parseFloat(inss) - parseFloat(sindicato)
-    
-
-    document.getElementById("bruto").innerHTML = "R$ " + bruto
-    document.getElementById("IR").innerHTML = "R$ " + ir
-    document.getElementById("INSS").innerHTML = "R$ " + inss
-    document.getElementById("Sindicato").innerHTML = "R$ " + sindicato
-    
-    document.getElementById("Liquido").innerHTML = "R$ " + total
-    
+    document.getElementById("bruto").innerHTML = "R$ " + total
 
 }
